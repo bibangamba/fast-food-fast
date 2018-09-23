77ca0eff1e35d84231c2114f097256e0d0ee7e45
bibangamba <andrewbibangamba@gmail.com>
2018-09-23 21:02:31 +0300

    heroku config changes 2 - requirements.txt

--- api_endpoints/run.py
+++ api_endpoints/run.py
@@ -2,13 +2,13 @@
 import os
 
 # from src import create_app #refers to create_app inside __init__.py
-from src.app import create_app #refers to create_app inside app.py
+from src.app import create_app  # refers to create_app inside app.py
 
 # use below command to set flask_env instead of set
 # $env:FLASK_ENV = "development"
 
-    env_name = os.getenv('FLASK_ENV')
-    app = create_app(env_name)
+env_name = os.getenv('FLASK_ENV')
+app = create_app(env_name)
 
 if __name__ == '__main__':
     env_name = os.getenv('PORT')
