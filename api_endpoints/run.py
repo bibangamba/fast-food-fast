06860a6531bca9854e8f4c5c46eeaf0a54e62b4a
bibangamba <andrewbibangamba@gmail.com>
2018-09-23 20:38:03 +0300

    heroku config changes

--- api_endpoints/run.py
+++ api_endpoints/run.py
@@ -7,8 +7,11 @@ from src.app import create_app #refers to create_app inside app.py
 # use below command to set flask_env instead of set
 # $env:FLASK_ENV = "development"
 
-if __name__ == '__main__':
     env_name = os.getenv('FLASK_ENV')
-    # print(env_name)
     app = create_app(env_name)
-    app.run()
+
+if __name__ == '__main__':
+    env_name = os.getenv('PORT')
+    # print(env_name)
+    # app.run()
+    app.run(host='0.0.0.0', port=port)
